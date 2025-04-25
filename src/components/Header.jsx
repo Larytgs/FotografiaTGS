3f785f019758804fd8728ffe83b1866033a44936
laryTgs <laryssatortato99@gmail.com>
2025-04-25 15:55:21 -0300

    up

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,13 +3,15 @@ import React from "react";
 
 const Header = () => {
   return (
-    <section className="flex">
-      <h1 className="font-black text-left">Fotografia Tgs</h1>
-      <div className="flex m-2 gap-3 text-rigth">
-        <a href="#">Home</a>
-        <a href="#">Beach</a>
-        <a href="#">Food</a>
-        <a href="#">Moments</a>
+    <section>
+      <div className="flex justify-around m-2">
+        <h1 className="font-black text-left m-5">Fotografia Tgs</h1>
+        <div className="mt-10 flex gap-6">
+          <a href="#">Home</a>
+          <a href="#">Beach</a>
+          <a href="#">Food</a>
+          <a href="#">Moments</a>
+        </div>
       </div>
     </section>
   );
