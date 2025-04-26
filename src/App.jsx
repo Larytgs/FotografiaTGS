09b55c71691b518f47b1b73d0399aa0bf4dd2a07
laryTgs <laryssatortato99@gmail.com>
2025-04-26 13:44:57 -0300

    up

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,11 @@
 //Componente PAI
 import "./App.css";
 import Header from "./components/Header/Header";
-import Beach from "./components/pages/Beach";
-import Food from "./components/pages/Food";
-import Animals from "./components/pages/Animals";
-import Moments from "./components/pages/Moments";
 
 function App() {
   return (
     <>
       <Header></Header>
-      <Beach></Beach>
-      <Food></Food>
-      <Animals></Animals>
-      <Moments></Moments>
     </>
   );
 }
---
+++ src/components/pages/Todos.jsx
@@ -0,0 +1,8 @@
+export { default as Animais } from "./Animals";
+export { default as Beach } from "./Beach";
+export { default as Food } from "./Food";
+export { default as Momentos } from "./Food";
+
+//Criei esse arquivo, para importar no Header.jsx, em uma linha só
+//Isso funciona porque o Todos.js agora serve como um ponto central de exportação.
+//Assim, o código fica mais limpo e organizado!
