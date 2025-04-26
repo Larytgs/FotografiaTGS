aecc5714a020c59a0bca3bd503df4aa1892307e6
laryTgs <laryssatortato99@gmail.com>
2025-04-26 14:33:55 -0300

    up

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,6 +4,8 @@ function Footer() {
     color: "black",
     backgroundColor: "#d4a14e9a",
     padding: "14px",
+    fontSize: "1.5em",
+    botton: "0",
   };
 
   return <h1 style={footerStyle}>&copy;2025 Laryssa Tortato Gomes da Silva</h1>;
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,19 +2,23 @@
 import { useState } from "react";
 import React from "react";
 
+import Home from "../Home/Home";
 import { Animals, Beach, Food, Moments } from "../pages/Todos";
 import Button, { Ul } from "./Style";
 import Footer from "../Footer/Footer";
 
 const Header = () => {
-  // Inicializando "presentation" como a seção visível
+  // Inicializando "home" como a seção visível
   // A variável "visibleSection" irá armazenar a seção visível atualmente (ou null se nenhuma)
-  const [visibleSection, setVisibleSection] = useState("presentation");
+  const [visibleSection, setVisibleSection] = useState("beach");
 
   // A função toggleSection vai alternar entre mostrar e esconder a seção
   const toggleSection = (section) => {
     // Se a seção clicada for a mesma que já está visível, ela permanece, senão, troca
     setVisibleSection(visibleSection === section ? visibleSection : section);
+    if (visibleSection !== section) {
+      setVisibleSection(section);
+    }
   };
 
   return (
@@ -23,9 +27,9 @@ const Header = () => {
         <div className="flex justify-around m-2">
           <h1 className="font-black text-left m-5">Fotografia Tgs</h1>
           <Ul>
-            <li>
-              <Button>Home</Button>
-            </li>
+            {/* <li>
+              <Button onClick={() => toggleSection("home")}>Home</Button>
+            </li> */}
             <li>
               <Button onClick={() => toggleSection("beach")}>Beach</Button>
             </li>
@@ -42,6 +46,7 @@ const Header = () => {
         </div>
 
         <div>
+          {visibleSection === "home" && <Home />}
           {visibleSection === "beach" && <Beach />}
           {visibleSection === "food" && <Food />}
           {visibleSection === "moments" && <Moments />}
