0a51d6c1ddb316b85a4818c83ca7744f7495642a
laryTgs <laryssatortato99@gmail.com>
2025-04-28 16:27:09 -0300

    up

--- src/components/pages/Animals.jsx
+++ src/components/pages/Animals.jsx
@@ -5,232 +5,232 @@ function Animals() {
     <section className="m-0 p-4 max-w-[100vw] bg-[#A9CA76]">
       {/* <h3 className="text-center font-bold mb-5">Fotos de Animais</h3> */}
       <div className="columns-2 md:columns-3 lg:columns-4 gap-4 space-y-4">
-        <img
+        <ZoomableImage
           className="w-full object-cover rounded-md shadow"
           src="/imgs/animais/1.jpg"
           alt=""
         />
-        <img
+        <ZoomableImage
           className="w-full object-cover rounded-md shadow"
           src="/imgs/animais/2.jpg"
           alt=""
         />
-        <img
+        <ZoomableImage
           className="w-full object-cover rounded-md shadow"
           src="/imgs/animais/3.jpeg"
           alt=""
         />
-        <img
+        <ZoomableImage
           className="w-full object-cover rounded-md shadow"
           src="/imgs/animais/4.jpg"
           alt=""
         />
-        <img
+        <ZoomableImage
           className="w-full object-cover rounded-md shadow"
           src="/imgs/animais/5.jpeg"
           alt=""
         />
-        <img
+        <ZoomableImage
           className="w-full object-cover rounded-md shadow"
           src="/imgs/animais/6.jpeg"
           alt=""
         />
-        <img
+        <ZoomableImage
           className="w-full object-cover rounded-md shadow"
           src="/imgs/animais/7.png"
           alt=""
         />
-        <img
+        <ZoomableImage
           className="w-full object-cover rounded-md shadow"
           src="/imgs/animais/8.jpeg"
           alt=""
         />
-        <img
+        <ZoomableImage
           className="w-full object-cover rounded-md shadow"
           src="/imgs/animais/9.jpeg"
           alt=""
         />
-        <img
+        <ZoomableImage
           className="w-full object-cover rounded-md shadow"
           src="/imgs/animais/10.jpeg"
           alt=""
         />
-        <img
+        <ZoomableImage
           className="w-full object-cover rounded-md shadow"
           src="/imgs/animais/11.jpg"
           alt=""
         />
-        <img
+        <ZoomableImage
           className="w-full object-cover rounded-md shadow"
           src="/imgs/animais/12.jpeg"
           alt=""
         />
-        <img
+        <ZoomableImage
           className="w-full object-cover rounded-md shadow"
           src="/imgs/animais/13.jpeg"
           alt=""
         />
-        <img
+        <ZoomableImage
           className="w-full object-cover rounded-md shadow"
           src="/imgs/animais/14.jpeg"
           alt=""
         />
-        <img
+        <ZoomableImage
           className="w-full object-cover rounded-md shadow"
           src="/imgs/animais/15.png"
           alt=""
         />
-        <img
+        <ZoomableImage
           className="w-full object-cover rounded-md shadow"
           src="/imgs/animais/16.jpeg"
           alt=""
         />
-        <img
+        <ZoomableImage
           className="w-full object-cover rounded-md shadow"
           src="/imgs/animais/17.jpeg"
           alt=""
         />
-        <img
+        <ZoomableImage
           className="w-full object-cover rounded-md shadow"
           src="/imgs/animais/18.jpeg"
           alt=""
         />
-        <img
+        <ZoomableImage
           className="w-full object-cover rounded-md shadow"
           src="/imgs/animais/19.jpeg"
           alt=""
         />
-        <img
+        <ZoomableImage
           className="w-full object-cover rounded-md shadow"
           src="/imgs/animais/20.jpg"
           alt=""
         />
-        <img
+        <ZoomableImage
           className="w-full object-cover rounded-md shadow"
           src="/imgs/animais/21.jpeg"
           alt=""
         />
-        <img
+        <ZoomableImage
           className="w-full object-cover rounded-md shadow"
           src="/imgs/animais/22.jpg"
           alt=""
         />
-        <img
+        <ZoomableImage
           className="w-full object-cover rounded-md shadow"
           src="/imgs/animais/23.jpeg"
           alt=""
         />
-        <img
+        <ZoomableImage
           className="w-full object-cover rounded-md shadow"
           src="/imgs/animais/24.jpeg"
           alt=""
         />
-        <img
+        <ZoomableImage
           className="w-full object-cover rounded-md shadow"
           src="/imgs/animais/25.jpeg"
           alt=""
         />
-        <img
+        <ZoomableImage
           className="w-full object-cover rounded-md shadow"
           src="/imgs/animais/26.jpg"
           alt=""
         />
-        <img
+        <ZoomableImage
           className="w-full object-cover rounded-md shadow"
           src="/imgs/animais/27.jpeg"
           alt=""
         />
-        <img
+        <ZoomableImage
           className="w-full object-cover rounded-md shadow"
           src="/imgs/animais/28.jpeg"
           alt=""
         />
-        <img
+        <ZoomableImage
           className="w-full object-cover rounded-md shadow"
           src="/imgs/animais/29.jpeg"
           alt=""
         />
-        <img
+        <ZoomableImage
           className="w-full object-cover rounded-md shadow"
           src="/imgs/animais/30.jpeg"
           alt=""
         />
-        <img
+        <ZoomableImage
           className="w-full object-cover rounded-md shadow"
           src="/imgs/animais/31.jpg"
           alt=""
         />
-        <img
+        <ZoomableImage
           className="w-full object-cover rounded-md shadow"
           src="/imgs/animais/32.jpeg"
           alt=""
         />
-        <img
+        <ZoomableImage
           className="w-full object-cover rounded-md shadow"
           src="/imgs/animais/33.jpeg"
           alt=""
         />
-        <img
+        <ZoomableImage
           className="w-full object-cover rounded-md shadow"
           src="/imgs/animais/34.jpeg"
           alt=""
         />
-        <img
+        <ZoomableImage
           className="w-full object-cover rounded-md shadow"
           src="/imgs/animais/35.jpeg"
           alt=""
         />
-        <img
+        <ZoomableImage
           className="w-full object-cover rounded-md shadow"
           src="/imgs/animais/36.png"
           alt=""
         />
-        <img
+        <ZoomableImage
           className="w-full object-cover rounded-md shadow"
           src="/imgs/animais/37.jpeg"
           alt=""
         />
-        <img
+        <ZoomableImage
           className="w-full object-cover rounded-md shadow"
           src="/imgs/animais/38.jpeg"
           alt=""
         />
-        <img
+        <ZoomableImage
           className="w-full object-cover rounded-md shadow"
           src="/imgs/animais/39.jpeg"
           alt=""
         />
-        <img
+        <ZoomableImage
           className="w-full object-cover rounded-md shadow"
           src="/imgs/animais/40.jpeg"
           alt=""
         />
-        <img
+        <ZoomableImage
           className="w-full object-cover rounded-md shadow"
           src="/imgs/animais/41.jpg"
           alt=""
         />
-        <img
+        <ZoomableImage
           className="w-full object-cover rounded-md shadow"
           src="/imgs/animais/42.png"
           alt=""
         />
-        <img
+        <ZoomableImage
           className="w-full object-cover rounded-md shadow"
           src="/imgs/animais/43.jpeg"
           alt=""
         />
-        <img
+        <ZoomableImage
           className="w-full object-cover rounded-md shadow"
           src="/imgs/animais/44.jpeg"
           alt=""
         />
-        <img
+        <ZoomableImage
           className="w-full object-cover rounded-md shadow"
           src="/imgs/animais/45.jpg"
           alt=""
         />
-        <img
+        <ZoomableImage
           className="w-full object-cover rounded-md shadow"
           src="/imgs/animais/46.jpeg"
           alt=""
