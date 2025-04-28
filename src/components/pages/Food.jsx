e7162b5ae54f84973f1c72e47cacbf321774c3ea
laryTgs <laryssatortato99@gmail.com>
2025-04-28 16:25:40 -0300

    Update Food.jsx

--- src/components/pages/Food.jsx
+++ src/components/pages/Food.jsx
@@ -5,202 +5,202 @@ function Food() {
     <section className="m-0 p-4 max-w-[100vw] bg-[#60210C]">
       {/* <h3 className="text-center font-bold mb-5">Fotos de comida</h3> */}
       <div className="columns-2 md:columns-3 lg:columns-4 gap-4 space-y-4">
-        <img
+        <ZoomableImage
           className="w-full object-cover rounded-md shadow"
           src="/imgs/comida/1.jpg"
           alt=""
         />
-        <img
+        <ZoomableImage
           className="w-full object-cover rounded-md shadow"
           src="/imgs/comida/2.jpg"
           alt=""
         />
-        <img
+        <ZoomableImage
           className="w-full object-cover rounded-md shadow"
           src="/imgs/comida/3.jpg"
           alt=""
         />
-        <img
+        <ZoomableImage
           className="w-full object-cover rounded-md shadow"
           src="/imgs/comida/4.jpg"
           alt=""
         />
-        <img
+        <ZoomableImage
           className="w-full object-cover rounded-md shadow"
           src="/imgs/comida/5.jpg"
           alt=""
         />
-        <img
+        <ZoomableImage
           className="w-full object-cover rounded-md shadow"
           src="/imgs/comida/6.jpg"
           alt=""
         />
-        <img
+        <ZoomableImage
           className="w-full object-cover rounded-md shadow"
           src="/imgs/comida/7.jpg"
           alt=""
         />
-        <img
+        <ZoomableImage
           className="w-full object-cover rounded-md shadow"
           src="/imgs/comida/8.jpg"
           alt=""
         />
-        <img
+        <ZoomableImage
           className="w-full object-cover rounded-md shadow"
           src="/imgs/comida/9.jpg"
           alt=""
         />
-        <img
+        <ZoomableImage
           className="w-full object-cover rounded-md shadow"
           src="/imgs/comida/10.jpg"
           alt=""
         />
-        <img
+        <ZoomableImage
           className="w-full object-cover rounded-md shadow"
           src="/imgs/comida/11.jpg"
           alt=""
         />
-        <img
+        <ZoomableImage
           className="w-full object-cover rounded-md shadow"
           src="/imgs/comida/12.jpg"
           alt=""
         />
-        <img
+        <ZoomableImage
           className="w-full object-cover rounded-md shadow"
           src="/imgs/comida/13.jpg"
           alt=""
         />
-        <img
+        <ZoomableImage
           className="w-full object-cover rounded-md shadow"
           src="/imgs/comida/14.jpg"
           alt=""
         />
-        <img
+        <ZoomableImage
           className="w-full object-cover rounded-md shadow"
           src="/imgs/comida/15.jpg"
           alt=""
         />
-        <img
+        <ZoomableImage
           className="w-full object-cover rounded-md shadow"
           src="/imgs/comida/16.jpg"
           alt=""
         />
-        <img
+        <ZoomableImage
           className="w-full object-cover rounded-md shadow"
           src="/imgs/comida/17.jpg"
           alt=""
         />
-        <img
+        <ZoomableImage
           className="w-full object-cover rounded-md shadow"
           src="/imgs/comida/18.jpg"
           alt=""
         />
-        <img
+        <ZoomableImage
           className="w-full object-cover rounded-md shadow"
           src="/imgs/comida/19.jpg"
           alt=""
         />
-        {/* <img
+        {/* <ZoomableImage
           className="w-full object-cover rounded-md shadow"
           src="/imgs/comida/20.jpg"
           alt=""
         /> */}
-        <img
+        <ZoomableImage
           className="w-full object-cover rounded-md shadow"
           src="/imgs/comida/21.jpg"
           alt=""
         />
-        <img
+        <ZoomableImage
           className="w-full object-cover rounded-md shadow"
           src="/imgs/comida/22.jpg"
           alt=""
         />
-        <img
+        <ZoomableImage
           className="w-full object-cover rounded-md shadow"
           src="/imgs/comida/23.jpg"
           alt=""
         />
-        <img
+        <ZoomableImage
           className="w-full object-cover rounded-md shadow"
           src="/imgs/comida/24.jpg"
           alt=""
         />
-        <img
+        <ZoomableImage
           className="w-full object-cover rounded-md shadow"
           src="/imgs/comida/25.jpg"
           alt=""
         />
-        <img
+        <ZoomableImage
           className="w-full object-cover rounded-md shadow"
           src="/imgs/comida/26.jpg"
           alt=""
         />
-        <img
+        <ZoomableImage
           className="w-full object-cover rounded-md shadow"
           src="/imgs/comida/27.jpg"
           alt=""
         />
-        <img
+        <ZoomableImage
           className="w-full object-cover rounded-md shadow"
           src="/imgs/comida/28.jpg"
           alt=""
         />
-        <img
+        <ZoomableImage
           className="w-full object-cover rounded-md shadow"
           src="/imgs/comida/29.jpg"
           alt=""
         />
-        <img
+        <ZoomableImage
           className="w-full object-cover rounded-md shadow"
           src="/imgs/comida/30.jpg"
           alt=""
         />
-        <img
+        <ZoomableImage
           className="w-full object-cover rounded-md shadow"
           src="/imgs/comida/31.jpg"
           alt=""
         />
-        <img
+        <ZoomableImage
           className="w-full object-cover rounded-md shadow"
           src="/imgs/comida/32.jpg"
           alt=""
         />
-        <img
+        <ZoomableImage
           className="w-full object-cover rounded-md shadow"
           src="/imgs/comida/33.jpg"
           alt=""
         />
-        <img
+        <ZoomableImage
           className="w-full object-cover rounded-md shadow"
           src="/imgs/comida/34.jpg"
           alt=""
         />
-        <img
+        <ZoomableImage
           className="w-full object-cover rounded-md shadow"
           src="/imgs/comida/35.jpg"
           alt=""
         />
-        <img
+        <ZoomableImage
           className="w-full object-cover rounded-md shadow"
           src="/imgs/comida/36.jpg"
           alt=""
         />
-        <img
+        <ZoomableImage
           className="w-full object-cover rounded-md shadow"
           src="/imgs/comida/37.jpg"
           alt=""
         />
-        <img
+        <ZoomableImage
           className="w-full object-cover rounded-md shadow"
           src="/imgs/comida/38.jpg"
           alt=""
         />
-        <img
+        <ZoomableImage
           className="w-full object-cover rounded-md shadow"
           src="/imgs/comida/39.jpg"
           alt=""
         />
-        <img
+        <ZoomableImage
           className="w-full object-cover rounded-md shadow"
           src="/imgs/comida/40.jpg"
           alt=""
