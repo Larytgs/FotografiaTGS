89a2d1330c28a81ecf36fed81aea23d2b153a009
laryTgs <laryssatortato99@gmail.com>
2025-04-28 16:28:08 -0300

    up

--- src/components/pages/Moments.jsx
+++ src/components/pages/Moments.jsx
@@ -35,7 +35,7 @@
 //           key={index}
 //           className="flex-shrink-0 w-screen h-screen flex items-center justify-center snap-center"
 //         >
-//           <img
+//           <ZoomableImage
 //             src={src}
 //             alt={`Imagem ${index}`}
 //             className="w-[80%] h-auto object-cover rounded-2xl shadow-lg"
@@ -47,6 +47,7 @@
 // }
 
 import React from "react";
+import ZoomableImage from "../Zoom/ZoomableImage";
 
 function Moments() {
   return (
@@ -58,214 +59,214 @@ function Moments() {
           lg:columns-4 → 4 colunas no desktop
           space-y-4 → espaço vertical entre as imagens
           */}
-        <img
+        <ZoomableImage
           //   Cada imagem ocupa 100% da largura da coluna (w-full), mas a altura respeita o formato da imagem.
           //   Resultado: um efeito de encaixe perfeito, igualzinho Pinterest
           className="w-full object-cover rounded-md shadow"
           src="/imgs/momentos/1.jpg"
           alt=""
         />
-        <img
+        <ZoomableImage
           className="w-full object-cover rounded-md shadow"
           src="/imgs/momentos/2.jpg"
           alt=""
         />
-        <img
+        <ZoomableImage
           className="w-full object-cover rounded-md shadow"
           src="/imgs/momentos/3.jpg"
           alt=""
         />
-        <img
+        <ZoomableImage
           className="w-full object-cover rounded-md shadow"
           src="/imgs/momentos/4.jpg"
           alt=""
         />
-        <img
+        <ZoomableImage
           className="w-full object-cover rounded-md shadow"
           src="/imgs/momentos/5.jpg"
           alt=""
         />
-        <img
+        <ZoomableImage
           className="w-full object-cover rounded-md shadow"
           src="/imgs/momentos/6.jpg"
           alt=""
         />
-        <img
+        <ZoomableImage
           className="w-full object-cover rounded-md shadow"
           src="/imgs/momentos/7.jpg"
           alt=""
         />
-        <img
+        <ZoomableImage
           className="w-full object-cover rounded-md shadow"
           src="/imgs/momentos/8.jpg"
           alt=""
         />
-        <img
+        <ZoomableImage
           className="w-full object-cover rounded-md shadow"
           src="/imgs/momentos/9.jpg"
           alt=""
         />
-        <img
+        <ZoomableImage
           className="w-full object-cover rounded-md shadow"
           src="/imgs/momentos/10.jpg"
           alt=""
         />
-        <img
+        <ZoomableImage
           className="w-full object-cover rounded-md shadow"
           src="/imgs/momentos/11.jpg"
           alt=""
         />
-        <img
+        <ZoomableImage
           className="w-full object-cover rounded-md shadow"
           src="/imgs/momentos/12.jpg"
           alt=""
         />
-        <img
+        <ZoomableImage
           className="w-full object-cover rounded-md shadow"
           src="/imgs/momentos/13.jpg"
           alt=""
         />
-        <img
+        <ZoomableImage
           className="w-full object-cover rounded-md shadow"
           src="/imgs/momentos/14.jpg"
           alt=""
         />
-        <img
+        <ZoomableImage
           className="w-full object-cover rounded-md shadow"
           src="/imgs/momentos/15.jpg"
           alt=""
         />
-        <img
+        <ZoomableImage
           className="w-full object-cover rounded-md shadow"
           src="/imgs/momentos/16.jpg"
           alt=""
         />
-        <img
+        <ZoomableImage
           className="w-full object-cover rounded-md shadow"
           src="/imgs/momentos/17.jpg"
           alt=""
         />
-        <img
+        <ZoomableImage
           className="w-full object-cover rounded-md shadow"
           src="/imgs/momentos/18.jpg"
           alt=""
         />
-        <img
+        <ZoomableImage
           className="w-full object-cover rounded-md shadow"
           src="/imgs/momentos/19.jpg"
           alt=""
         />
-        <img
+        <ZoomableImage
           className="w-full object-cover rounded-md shadow"
           src="/imgs/momentos/20.jpg"
           alt=""
         />
-        <img
+        <ZoomableImage
           className="w-full object-cover rounded-md shadow"
           src="/imgs/momentos/21.jpg"
           alt=""
         />
-        <img
+        <ZoomableImage
           className="w-full object-cover rounded-md shadow"
           src="/imgs/momentos/22.jpg"
           alt=""
         />
-        <img
+        <ZoomableImage
           className="w-full object-cover rounded-md shadow"
           src="/imgs/momentos/23.jpg"
           alt=""
         />
-        <img
+        <ZoomableImage
           className="w-full object-cover rounded-md shadow"
           src="/imgs/momentos/24.jpg"
           alt=""
         />
-        <img
+        <ZoomableImage
           className="w-full object-cover rounded-md shadow"
           src="/imgs/momentos/25.jpg"
           alt=""
         />
-        <img
+        <ZoomableImage
           className="w-full object-cover rounded-md shadow"
           src="/imgs/momentos/26.jpg"
           alt=""
         />
-        <img
+        <ZoomableImage
           className="w-full object-cover rounded-md shadow"
           src="/imgs/momentos/27.jpg"
           alt=""
         />
-        <img
+        <ZoomableImage
           className="w-full object-cover rounded-md shadow"
           src="/imgs/momentos/28.jpg"
           alt=""
         />
-        <img
+        <ZoomableImage
           className="w-full object-cover rounded-md shadow"
           src="/imgs/momentos/29.jpg"
           alt=""
         />
-        <img
+        <ZoomableImage
           className="w-full object-cover rounded-md shadow"
           src="/imgs/momentos/30.jpg"
           alt=""
         />
-        <img
+        <ZoomableImage
           className="w-full object-cover rounded-md shadow"
           src="/imgs/momentos/31.jpg"
           alt=""
         />
-        <img
+        <ZoomableImage
           className="w-full object-cover rounded-md shadow"
           src="/imgs/momentos/32.jpg"
           alt=""
         />
-        {/* <img
+        {/* <ZoomableImage
         className="w-full object-cover rounded-md shadow"
         src="/imgs/momentos/33.jpg"
         alt=""
       /> */}
-        <img
+        <ZoomableImage
           className="w-full object-cover rounded-md shadow"
           src="/imgs/momentos/34.jpg"
           alt=""
         />
-        <img
+        <ZoomableImage
           className="w-full object-cover rounded-md shadow"
           src="/imgs/momentos/35.jpg"
           alt=""
         />
-        <img
+        <ZoomableImage
           className="w-full object-cover rounded-md shadow"
           src="/imgs/momentos/36.jpg"
           alt=""
         />
-        <img
+        <ZoomableImage
           className="w-full object-cover rounded-md shadow"
           src="/imgs/momentos/37.jpg"
           alt=""
         />
-        <img
+        <ZoomableImage
           className="w-full object-cover rounded-md shadow"
           src="/imgs/momentos/38.jpg"
           alt=""
         />
-        <img
+        <ZoomableImage
           className="w-full object-cover rounded-md shadow"
           src="/imgs/momentos/39.jpg"
           alt=""
         />
-        <img
+        <ZoomableImage
           className="w-full object-cover rounded-md shadow"
           src="/imgs/momentos/40.jpg"
           alt=""
         />
-        <img
+        <ZoomableImage
           className="w-full object-cover rounded-md shadow"
           src="/imgs/momentos/41.jpg"
           alt=""
         />
-        <img
+        <ZoomableImage
           className="w-full object-cover rounded-md shadow"
           src="/imgs/momentos/42.jpg"
           alt=""
